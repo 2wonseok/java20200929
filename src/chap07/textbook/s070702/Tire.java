d901aa8154a972ad9f3c9b4d2bd9bec362f59585
2wonseok <lws3793@naver.com>
2020-10-15 17:40:06 +0900

    10/15 오후 수업 푸시

--- src/chap07/textbook/s070702/Tire.java
+++ src/chap07/textbook/s070702/Tire.java
@@ -2,7 +2,7 @@ package chap07.textbook.s070702;
 
 public class Tire {
 	public int maxRotation;			// 최대 회전수(타이어 수명)
-	public int accumlatedRotation;	// 누적 회전수
+	public int accumulatedRotation;	// 누적 회전수
 	public String location;			// 타이어의 위치
 	
 	public Tire(String location, int maxRotation) {
@@ -11,10 +11,10 @@ public class Tire {
 	}
 	
 	public boolean roll() {
-		++accumlatedRotation; // 누적 회전수 1 증가
-		if (accumlatedRotation < maxRotation) {
+		++accumulatedRotation; // 누적 회전수 1 증가
+		if (accumulatedRotation < maxRotation) {
 			System.out.println(location + "Tire 수명: " 
-								+ maxRotation - accumlatedRotation + "회");
+								+ (maxRotation - accumulatedRotation) + "회");
 			return true;
 		} else {
 			System.out.println("*** " + location + "Tire 펑크 ***");
---
+++ src/chap07/textbook/s070704/Texi.java
@@ -0,0 +1,8 @@
+package chap07.textbook.s070704;
+
+public class Texi extends Vehicle {
+	@Override
+	public void run() {
+		System.out.println("택시가 달립니다.");
+	}
+}
