b0b5e805ad466251e42464ba3ec79216db224299
2wonseok <lws3793@naver.com>
2020-10-23 12:39:11 +0900

    10/23 오전 푸시

---
+++ src/chap15/textbook/s150202/Board.java
@@ -0,0 +1,15 @@
+package chap15.textbook.s150202;
+
+public class Board {
+
+	public String subject;
+	public String content;
+	public String writer;
+
+	public Board(String subject, String content, String writer) {
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
+	}
+
+}
